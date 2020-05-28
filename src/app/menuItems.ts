497b90eac7fae8b9808b7de60dd8d15f2bd25eed
Riley Wong <rlwong2@gmail.com>
2020-05-28 11:15:02 -0500

    Added all menu items ingredient quantities

--- src/app/menuItems.ts
+++ src/app/menuItems.ts
@@ -7,7 +7,10 @@ export const MENUITEMS: menuItem[] = [
         'Expresso': 0,
         'Sugar': 1,
         'Cream': 1,
-        'Steamed Milk': 0
+        'Steamed Milk': 0,
+        'Foamed Milk': 0,
+        'Cocoa': 0,
+        'Whipped Cream': 0
     }},
     { id: 2, name: 'Decaf Coffee', ingredients: {
         'Coffee': 0,
@@ -15,14 +18,53 @@ export const MENUITEMS: menuItem[] = [
         'Expresso': 0,
         'Sugar': 1,
         'Cream': 1,
-        'Steamed Milk': 0
+        'Steamed Milk': 0,
+        'Foamed Milk': 0,
+        'Cocoa': 0,
+        'Whipped Cream': 0
     }},
     { id: 3, name: 'Caffe Latte', ingredients: {
         'Coffee': 0,
-        'Decaf Coffee': 3,
-        'Expresso': 0,
-        'Sugar': 1,
-        'Cream': 1,
-        'Steamed Milk': 0
+        'Decaf Coffee': 0,
+        'Expresso': 2,
+        'Sugar': 0,
+        'Cream': 0,
+        'Steamed Milk': 1,
+        'Foamed Milk': 0,
+        'Cocoa': 0,
+        'Whipped Cream': 0
+    }},
+    { id: 4, name: 'Caffe Americano', ingredients: {
+        'Coffee': 0,
+        'Decaf Coffee': 0,
+        'Expresso': 3,
+        'Sugar': 0,
+        'Cream': 0,
+        'Steamed Milk': 0,
+        'Foamed Milk': 0,
+        'Cocoa': 0,
+        'Whipped Cream': 0
+    }},
+    { id: 5, name: 'Caffe Mocha', ingredients: {
+        'Coffee': 0,
+        'Decaf Coffee': 0,
+        'Expresso': 1,
+        'Sugar': 0,
+        'Cream': 0,
+        'Steamed Milk': 1,
+        'Foamed Milk': 0,
+        'Cocoa': 1,
+        'Whipped Cream': 1
+    }},
+    { id: 6, name: 'Cappuccino', ingredients: {
+        'Coffee': 0,
+        'Decaf Coffee': 0,
+        'Expresso': 2,
+        'Sugar': 0,
+        'Cream': 0,
+        'Steamed Milk': 1,
+        'Foamed Milk': 1,
+        'Cocoa': 0,
+        'Whipped Cream': 0
     }},
 ];
